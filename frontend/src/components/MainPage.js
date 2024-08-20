8fe5eaac136c9998263b9cd6c57ac4b98244383a
Meet1903 <meet.diwan.md@gmail.com>
2024-08-20 15:03:27 -0400

    chore: Add MainPage component to frontend

---
+++ frontend/src/components/MainPage.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import careerStarLogo from '../assets/images/CareerStarlogowhite.png';
+import astroaut from '../assets/images/main-page-astronaut.png';
+
+function MainPage() {
+    return (
+        <div className='main-page'>
+            <div className='career-star-logo'>
+                <img src={careerStarLogo} alt='Career Star Logo' />
+            </div>
+            <div className='main-page-contnent'>
+                <div className='hey'>
+                    <img src={astroaut} alt='Astronaut' />
+                </div>
+
+                <div className='main-page-header'>
+                    <h1>Start owning your</h1>
+                    <h1>career journey</h1>
+                    <p>Personalized & motivating professional development tool that puts people first. Built with love. </p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default MainPage;
