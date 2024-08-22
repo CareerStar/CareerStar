eba58f07368adb6c57e6b036feef45ef0a0346f1
Meet1903 <meet.diwan.md@gmail.com>
2024-08-21 23:12:30 -0400

    feat: Add EmailCredentialPage component for email credential input in SignUp process

---
+++ frontend/src/components/EmailCredentialPage.js
@@ -0,0 +1,64 @@
+import React, {useState} from 'react';
+import { Navigate, useNavigate } from 'react-router-dom';
+import ProgressBar from './ProgressBar';
+import careerStarLogo from '../assets/images/career-star-logo-black.png';
+
+function EmailCredentialPage() {
+    const currentStep = 3;
+
+    const [isChecked, setIsChecked] = useState(true);
+
+    const handleToggle = () => {
+        setIsChecked(!isChecked);
+    };
+
+    const navigate = useNavigate();
+
+    const nextPageNavigation = () => {
+        console.log('Div clicked!');
+        navigate('/userIntent');
+    }
+
+    const navigateToMainPage = () => {
+        navigate('/');
+    }
+
+    return (
+        <div className='signUp-page'>
+            <div className='career-star-logo' onClick={navigateToMainPage}>
+                <img src={careerStarLogo} alt='Career Star Logo' />
+            </div>
+            <div className='signUp-page-content'>
+                <ProgressBar currentStep={currentStep} />
+                <div className='signUp-page-question'>
+                    <h2>Create <span className="highlight">my account</span></h2>
+                    <p>Email address</p>
+                    <input type='text' placeholder='abigail@gmail.com' />
+                    <p>Password</p>
+                    <input type='text' placeholder='**********' />
+                </div>
+                <div className='toggle-button-container'>
+                    <div
+                        className={`toggle-button ${isChecked ? 'checked' : ''}`}
+                        onClick={handleToggle}
+                    >
+                    </div>
+                    <p>Yes, I’d like to receive updates and events from CareerStar</p>
+                </div>
+                <div className='signUp-page-button' onClick={nextPageNavigation}>
+                    <p>Continue</p>
+                </div>
+
+                <div className='signUp-page-footer'>
+                    <p>Have an account? <a href='https://google.com' target='_blank'>Log in</a></p>
+                </div>
+
+                <div className='email-credential-page-footer'>
+                By signing up, you accept <span className='email-credential-page-footer-bold'>Terms & Conditions</span>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default EmailCredentialPage;
