9b125692c4008cffc1f6e10054ba5e0d70275f1a
Meet1903 <meet.diwan.md@gmail.com>
2024-08-21 20:32:10 -0400

    feat: Add react-router-dom for routing in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 import MainPage from './components/MainPage';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+import SignUp from './components/SignUp';
 
 function App() {
   return (
     <div className="App">
-      <MainPage />
+      <Router>
+        <Routes>
+
+          <Route
+            exact
+            path="/"
+            element={<MainPage />}
+          />
+
+          <Route
+            exact
+            path="/signup"
+            element={<SignUp />}
+          />
+        </Routes>
+      </Router>
     </div>
   );
 }
