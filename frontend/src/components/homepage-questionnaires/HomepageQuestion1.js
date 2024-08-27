333ce8fabc8ba8c81e9430cbbf54f25890003a07
Meet1903 <meet.diwan.md@gmail.com>
2024-08-26 20:35:28 -0400

    Add HomepageQuestion1 component for homepage questionnaire

---
+++ frontend/src/components/homepage-questionnaires/HomepageQuestion1.js
@@ -0,0 +1,26 @@
+import React, {useState} from 'react';
+
+function HomepageQuestion1() {
+    const [selectedOption, setSelectedOption] = useState('');
+
+    const handleChange = (event) => {
+        setSelectedOption(event.target.value);
+    };
+
+    return (
+        <div className='home-page-question'>
+            <h2>This describes me best...</h2>
+            <div className=''>
+                <select id="options" className='home-page-dropdown-menu' value={selectedOption} onChange={handleChange}>
+                    <option value="" disabled>Select an option</option>
+                    <option value="option1">Option 1</option>
+                    <option value="option2">Option 2</option>
+                    <option value="option3">Option 3</option>
+                </select>
+                {selectedOption && <p>You selected: {selectedOption}</p>}
+            </div>
+        </div>
+    );
+}
+
+export default HomepageQuestion1;
