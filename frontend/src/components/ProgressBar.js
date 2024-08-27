4138d4e861a4ee54b94c3f5024532870c507249b
Meet1903 <meet.diwan.md@gmail.com>
2024-08-26 20:39:11 -0400

    Update ProgressBar component to support dynamic total steps

--- frontend/src/components/ProgressBar.js
+++ frontend/src/components/ProgressBar.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-function ProgressBar({ currentStep }) {
-    const progress = (currentStep / 3) * 100;
+function ProgressBar({ currentStep, totalSteps }) {
+    const progress = (currentStep / totalSteps) * 100;
 
     return (
         <div className='main-progress-container'>
             <div className="progress-container">
                 <div className="progress-bar" style={{ width: `${progress}%` }} />
             </div>
-            <p className="progress-text">{currentStep} of 3</p>
+            <p className="progress-text">{currentStep} of {totalSteps}</p>
         </div>
     );
 }
