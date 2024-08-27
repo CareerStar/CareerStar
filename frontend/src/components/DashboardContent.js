36be192554e831cf042cf6f93eb75195d7720384
Meet1903 <meet.diwan.md@gmail.com>
2024-08-27 10:07:45 -0400

    feat: Add dashboard content pages and update DashboardContent component

--- frontend/src/components/DashboardContent.js
+++ frontend/src/components/DashboardContent.js
@@ -1,21 +1,26 @@
 import React from 'react';
-import Home from './Home';
+import Home from './dashboard-content-pages/Home';
+import Profile from './dashboard-content-pages/Profile';
+import Roadmap from './dashboard-content-pages/Roadmap';
+import Events from './dashboard-content-pages/Events';
+import Network from './dashboard-content-pages/Network';
+import Support from './dashboard-content-pages/Support';
 
 function DashboardContent({selectedPage}) {
     const renderPage = () => {
         switch (selectedPage) {
             case 'Home':
                 return <Home />;
-            // case 'Profile':
-            //     return <Profile />;
-            // case 'Roadmap':
-            //     return <Roadmap />;
-            // case 'Events':
-            //     return <Events />;
-            // case 'Network':
-            //     return <Network />;
-            // case 'Support':
-            //     return <Support />;
+            case 'Profile':
+                return <Profile />;
+            case 'Roadmap':
+                return <Roadmap />;
+            case 'Events':
+                return <Events />;
+            case 'Network':
+                return <Network />;
+            case 'Support':
+                return <Support />;
             default:
                 return <Home />;
         }
