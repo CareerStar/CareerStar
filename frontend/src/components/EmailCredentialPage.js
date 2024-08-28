0d4f22a7513fcfcd7e642b329817919c94910388
Meet1903 <meet.diwan.md@gmail.com>
2024-08-27 23:02:38 -0400

    chore: Add popup component for email credential page.

--- frontend/src/components/EmailCredentialPage.js
+++ frontend/src/components/EmailCredentialPage.js
@@ -1,13 +1,15 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Navigate, useNavigate } from 'react-router-dom';
 import ProgressBar from './ProgressBar';
 import careerStarLogo from '../assets/images/career-star-logo-black.png';
+import Stars3 from '../assets/images/stars3.png';
 
 function EmailCredentialPage() {
     const currentStep = 3;
     const totalSteps = 3;
 
     const [isChecked, setIsChecked] = useState(true);
+    const [showPopup, setShowPopup] = useState(false);
 
     const handleToggle = () => {
         setIsChecked(!isChecked);
@@ -17,6 +19,12 @@ function EmailCredentialPage() {
 
     const nextPageNavigation = () => {
         console.log('Div clicked!');
+        setShowPopup(true);
+        // navigate('/dashboard');
+    }
+
+    const navigateToDashboard = () => {
+        setShowPopup(false);
         navigate('/dashboard');
     }
 
@@ -55,9 +63,24 @@ function EmailCredentialPage() {
                 </div>
 
                 <div className='email-credential-page-footer'>
-                By signing up, you accept <span className='email-credential-page-footer-bold'>Terms & Conditions</span>
+                    By signing up, you accept <span className='email-credential-page-footer-bold'>Terms & Conditions</span>
                 </div>
             </div>
+            
+            {showPopup && (
+                <div className='popup'>
+                    <div className='popup-content'>
+                        <img src={Stars3} alt='3 stars' />
+                        <div className='earned-star'>You earned 3 stars!</div>
+                        <div className='popup-text'>
+                            <p>You took the first step to owning your professional journey - let’s celebrate that small win!</p>
+                        </div>
+                        <div className='popup-submit-button' onClick={navigateToDashboard}>
+                            <p>Let's get started</p>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
