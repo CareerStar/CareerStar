c648830b8d0f96adb4c2c286e091e3732eb62539
Meet1903 <meet.diwan.md@gmail.com>
2024-08-28 21:18:04 -0400

    refactor: Update HomepageQuestion4 component to use onOptionSelect prop

--- frontend/src/components/homepage-questionnaires/HomepageQuestion4.js
+++ frontend/src/components/homepage-questionnaires/HomepageQuestion4.js
@@ -1,19 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-function HomepageQuestion4() {
-    const [selectedOption, setSelectedOption] = useState('');
-
-    const handleChange = (event) => {
-        setSelectedOption(event.target.value);
+function HomepageQuestion4({ onOptionSelect }) {
+    const handleOptionSelect = (option) => {
+        onOptionSelect(option);
     };
 
     return (
         <div className='home-page-question'>
             <h2>What do you need help with to reach that goal?</h2>
-            <div className='home-page-question-choice'>
+            <div className='home-page-question-choice' onClick={() => handleOptionSelect('roadmap')}>
                 <p>Build a career roadmap - I need clarity!</p>
             </div>
-            <div className='home-page-question-choice'>
+            <div className='home-page-question-choice' onClick={() => handleOptionSelect('activities')}>
                 <p>Do recommended activities - I need to get going!</p>
             </div>
         </div>
