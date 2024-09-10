155e79231fbc7accb325a2552e71219d10bb0c5f
Meet1903 <meet.diwan.md@gmail.com>
2024-09-10 11:04:13 -0400

    Update Roadmap page with goal cards and edit icon

--- frontend/src/components/dashboard-content-pages/Roadmap.js
+++ frontend/src/components/dashboard-content-pages/Roadmap.js
@@ -1,9 +1,21 @@
 import React from 'react';
-
+import editIcon from '../../assets/images/edit-icon.png';
 function Roadmap() {
     return (
-        <div>
-            <h1>Roadmap Page</h1>
+        <div className='roadmap-container'>
+            <p className='roadmap-title'>My Roadmap</p>
+            <div className='roadmap-goals-container flex-row'>
+                <div className='roadmap-goal-card flex-column'>
+                    <img src={editIcon} alt='Edit icon' />
+                    <p className='roadmap-goal-card-heading'>Where you’re at</p>
+                    <p className='roadmap-goal-card-answer'>I’m a recent grad</p>
+                </div>
+                <div className='roadmap-goal-card flex-column'>
+                    <img src={editIcon} alt='Edit icon' />
+                    <p className='roadmap-goal-card-heading'>The goal</p>
+                    <p className='roadmap-goal-card-answer'>To get my first full-time role as a software engineer</p>
+                </div>
+            </div>
         </div>
     );
 };
