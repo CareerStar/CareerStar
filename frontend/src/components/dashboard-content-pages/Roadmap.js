c78d1082a580ceb63531fbcff23ce8ba17428356
Meet1903 <meet.diwan.md@gmail.com>
2024-09-10 14:17:58 -0400

    Update Roadmap page

--- frontend/src/components/dashboard-content-pages/Roadmap.js
+++ frontend/src/components/dashboard-content-pages/Roadmap.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import editIcon from '../../assets/images/edit-icon.png';
+import starEmpty from '../../assets/images/star-empty.png';
+import downArrow from '../../assets/images/down-arrow-roadmap.png';
 function Roadmap() {
     return (
         <div className='roadmap-container'>
@@ -14,6 +16,41 @@ function Roadmap() {
                     <img src={editIcon} alt='Edit icon' />
                     <p className='roadmap-goal-card-heading'>The goal</p>
                     <p className='roadmap-goal-card-answer'>To get my first full-time role as a software engineer</p>
+                </div>
+            </div>
+
+            <div className='roadmap-phases-container'>
+                <div className='roadmap-phase flex-column'>
+                    <p className='roadmap-phase-title'>Phase 1: Values & Goals</p>
+                    <div>
+                        <div className='roadmap-sub-phase flex-row'>
+                            <div className='roadmap-phase-card'>
+                                <input
+                                    type="checkbox"
+                                />
+                                <p>Looking back <span className='bold'>1 year</span> from now, what do you want to have accomplished professionally?</p>
+                                <img src={downArrow} alt='Down arrow icon' />
+                            </div>
+                            <div className='roadmap-phase-star-count flex-row'>
+                                <p>5</p>
+                                <img src={starEmpty} alt='Star icon' />
+                            </div>
+                        </div>
+
+                        <div className='roadmap-sub-phase flex-row'>
+                            <div className='roadmap-phase-card'>
+                                <input
+                                    type="checkbox"
+                                />
+                                <p>Let’s identify your <span className='bold'>Values</span></p>
+                                <img src={downArrow} alt='Down arrow icon' />
+                            </div>
+                            <div className='roadmap-phase-star-count flex-row'>
+                                <p>5</p>
+                                <img src={starEmpty} alt='Star icon' />
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
