bdea1f41331158e8267a7e0517aab154264bbe1d
Meet1903 <meet.diwan.md@gmail.com>
2024-09-14 23:07:52 -0400

    Update SignUp component with form input handling for firstname

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -1,22 +1,27 @@
-import React from 'react';
-import { Navigate, useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import ProgressBar from './ProgressBar';
 import careerStarLogo from '../assets/images/career-star-logo-black.png';
 
 function SignUp() {
+    const [firstname, setFirstname] = useState('');
     const currentStep = 1;
     const totalSteps = 3;
 
-    const navigate = useNavigate(); 
+    const navigate = useNavigate();
+
+    const handleInputChange = (event) => {
+        setFirstname(event.target.value);
+    };
 
     const nextPageNavigation = () => {
         console.log('Div clicked!');
-        navigate('/userIntent');
-    }
+        navigate('/userIntent', { state: { firstname } });
+    };
 
     const navigateToStartPage = () => {
         navigate('/');
-    }
+    };
 
     return (
         <div className='signUp-page'>
@@ -24,11 +29,16 @@ function SignUp() {
                 <img src={careerStarLogo} alt='Career Star Logo' />
             </div>
             <div className='signUp-page-content'>
-                <ProgressBar currentStep={currentStep} totalSteps={totalSteps}/>
+                <ProgressBar currentStep={currentStep} totalSteps={totalSteps} />
                 <div className='signUp-page-question'>
                     <h2>What's your <span className="highlight">name?</span></h2>
                     <p>First name</p>
-                    <input type='text' placeholder='Your First name' />
+                    <input
+                        type='text'
+                        placeholder='Your First name'
+                        value={firstname}
+                        onChange={handleInputChange}
+                    />
                 </div>
                 <div className='signUp-page-button' onClick={nextPageNavigation}>
                     <p>Continue</p>
