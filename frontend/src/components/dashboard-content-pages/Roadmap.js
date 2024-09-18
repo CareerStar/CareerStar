c5a8bbde1466486f40c0b005ca5280384f69ad9c
Meet1903 <meet.diwan.md@gmail.com>
2024-09-18 16:47:02 -0400

    Update Roadmap component to include user onboarding functionality

--- frontend/src/components/dashboard-content-pages/Roadmap.js
+++ frontend/src/components/dashboard-content-pages/Roadmap.js
@@ -1,21 +1,130 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import editIcon from '../../assets/images/edit-icon.png';
 import starEmpty from '../../assets/images/star-empty.png';
 import downArrow from '../../assets/images/down-arrow-roadmap.png';
-function Roadmap() {
+
+function Roadmap({ userId }) {
+    const [currentSituation, setCurrentSituation] = useState('I’m a recent grad');
+    const [goal, setGoal] = useState('To get my first full-time role as a software engineer');
+    const [isEditingCurrentSituation, setIsEditingCurrentSituation] = useState(false);
+    const [isEditingGoal, setIsEditingGoal] = useState(false);
+    const inputGoalRef = useRef(null);
+    const inputSituationRef = useRef(null);
+
+    useEffect(() => {
+        const fetchUserDetails = async () => {
+            try {
+                const response = await fetch(`http://127.0.0.1:5000/onboarding/${userId}`);
+                const data = await response.json();
+                if (response.ok) {
+                    setCurrentSituation(data.currentSituation);
+                    setGoal(data.goal);
+                    console.log('User onboarding details:', data);
+                } else {
+                    console.error('Error fetching user details:', data);
+                }
+            } catch (error) {
+                console.error('Error fetching user details:', error);
+            }
+        }
+        if (userId) {
+            fetchUserDetails();
+        }
+    }, [userId]);
+
+    // Handle save function for current situation and goal
+    const saveUserDetails = async () => {
+        try {
+            const response = await fetch(`http://127.0.0.1:5000/update_onboarding/${userId}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    "currentSituation": currentSituation,
+                    "goal": goal
+                }),
+            });
+
+            if (response.ok) {
+                console.log('User details updated successfully');
+            } else {
+                console.error('Error updating user details');
+            }
+        } catch (error) {
+            console.error('Error updating user details:', error);
+        }
+    };
+
+    const handleCurrentSituationEdit = () => {
+        setIsEditingCurrentSituation(true);
+        setTimeout(() => {
+            if (inputSituationRef.current) {
+                inputSituationRef.current.focus();
+                inputSituationRef.current.setSelectionRange(currentSituation.length, currentSituation.length);  // Move cursor to the end
+            }
+        }, 0);
+    };
+
+    const handleGoalEdit = () => {
+        setIsEditingGoal(true);
+        setTimeout(() => {
+            if (inputGoalRef.current) {
+                inputGoalRef.current.focus();
+                inputGoalRef.current.setSelectionRange(goal.length, goal.length);  // Move cursor to the end
+            }
+        }, 0);
+    };
+
+    const handleSaveCurrentSituation = () => {
+        setIsEditingCurrentSituation(false);
+        saveUserDetails();
+    };
+
+    const handleSaveGoal = () => {
+        setIsEditingGoal(false);
+        saveUserDetails();
+    };
+
     return (
         <div className='roadmap-container'>
             <p className='roadmap-title'>My Roadmap</p>
             <div className='roadmap-goals-container flex-row'>
                 <div className='roadmap-goal-card flex-column'>
-                    <img src={editIcon} alt='Edit icon' />
+                    <img src={editIcon} alt='Edit icon' onClick={handleCurrentSituationEdit} />
                     <p className='roadmap-goal-card-heading'>Where you’re at</p>
-                    <p className='roadmap-goal-card-answer'>I’m a recent grad</p>
+                    {isEditingCurrentSituation ? (
+                        <div>
+                            <input
+                                ref={inputSituationRef}
+                                className='roadmap-goal-card-input'
+                                type="text"
+                                value={currentSituation}
+                                onChange={(e) => setCurrentSituation(e.target.value)}
+                            />
+                            <button onClick={handleSaveCurrentSituation} className='situation-goal-save-button'>Save</button>
+                        </div>
+                    ) : (
+                        <p className='roadmap-goal-card-answer'>{currentSituation}</p>
+                    )}
                 </div>
                 <div className='roadmap-goal-card flex-column'>
-                    <img src={editIcon} alt='Edit icon' />
+                    <img src={editIcon} alt='Edit icon' onClick={handleGoalEdit} />
                     <p className='roadmap-goal-card-heading'>The goal</p>
-                    <p className='roadmap-goal-card-answer'>To get my first full-time role as a software engineer</p>
+                    {isEditingGoal ? (
+                        <div>
+                            <input
+                                ref={inputGoalRef}
+                                className='roadmap-goal-card-input'
+                                type="text"
+                                value={goal}
+                                onChange={(e) => setGoal(e.target.value)}
+                            />
+                            <button onClick={handleSaveGoal} className='situation-goal-save-button'>Save</button>
+                        </div>
+                    ) : (
+                        <p className='roadmap-goal-card-answer'>{goal}</p>
+                    )}
                 </div>
             </div>
 
