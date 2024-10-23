5fbf8bce67e12862b24fde6ba7fc0a3922b081d2
Meet1903 <meet.diwan.md@gmail.com>
2024-10-22 23:27:15 -0400

    Update StartPage header with new title and description

--- frontend/src/components/StartPage.js
+++ frontend/src/components/StartPage.js
@@ -28,9 +28,8 @@ function StartPage() {
                 </div>
 
                 <div className='main-page-header'>
-                    <h1>Start owning your</h1>
-                    <h1>career journey</h1>
-                    <p>Personalized & motivating professional development tool that puts people first. Built with love. </p>
+                    <h1>Your LaunchPad to Success</h1>
+                    <p>Own your career and land your next role with a personalized roadmap and the support of our AllStar community. Built with love. 💜</p>
                 </div>
 
                 <div className='main-page-button' onClick={handleClick}>
