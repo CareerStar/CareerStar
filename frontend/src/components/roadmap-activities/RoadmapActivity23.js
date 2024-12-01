0c29b10b847f3ba3f1a17c084e7c934a19d88d56
Meet1903 <meet.diwan.md@gmail.com>
2024-12-01 17:32:04 -0500

    Updated Roadmap Phase 2 Activity 3

---
+++ frontend/src/components/roadmap-activities/RoadmapActivity23.js
@@ -0,0 +1,140 @@
+import React, { useEffect, useState, useRef } from 'react';
+import axios from 'axios';
+import starEmpty from '../../assets/images/star-empty.png';
+import star from '../../assets/images/star.png';
+import downArrow from '../../assets/images/down-arrow-roadmap.png';
+import upArrow from '../../assets/images/up-arrow-roadmap.png';
+import TypeformEmbed from './TypeformEmbed';
+
+function RoadmapActivity23({ userId }) {
+    const activityId = 23;
+    const [completed, setCompleted] = useState(false);
+    const [starCount, setStarCount] = useState(3);
+    const [answers, setAnswers] = useState({
+        coldMessage: '', //Answers can not be a null JSON object, it has to have at least one key-value pair
+    });
+    const [isDescriptionVisible, setIsDescriptionVisible] = useState(false);
+
+    useEffect(() => {
+        const fetchUserDetails = async () => {
+            try {
+                const response = await axios.get(`https://ec2-34-227-29-26.compute-1.amazonaws.com:5000/roadmapactivity/${userId}/${activityId}`);
+                if (response.data) {
+                    setAnswers(response.data[0]);
+                    setCompleted(response.data[1]);
+                }
+            } catch (error) {
+                console.error('Activity not completed', error);
+            }
+        };
+        if (userId) {
+            fetchUserDetails();
+        }
+    }, [userId]);
+
+    const handleSubmit = async (completed) => {
+        try {
+            const payload = {
+                userId: userId,
+                roadmapActivityId: 23,
+                completed: completed,
+                answers: answers,
+                stars: starCount,
+            };
+            const response = await axios.post(`https://ec2-34-227-29-26.compute-1.amazonaws.com:5000/roadmapactivity/${userId}/${activityId}`, payload);
+            if (response.status === 200) {
+                console.log(response.data.message);
+                if (completed) {
+                    setCompleted(true);
+                }
+                toggleDescriptionVisibility();
+                window.location.reload();
+            }
+        } catch (error) {
+            console.error('Error saving answers:', error);
+        }
+    };
+
+    const toggleDescriptionVisibility = () => {
+        setIsDescriptionVisible(!isDescriptionVisible);
+    };
+
+    return (
+
+        <div>
+            <div className='roadmap-sub-phase flex-row'>
+                <div className='roadmap-phase-card'>
+                    <input
+                        type="checkbox"
+                        checked={completed}
+                    />
+                    <p>Cold Outreach Message 1 - Compare & Contrast 🎱</p>
+                    {isDescriptionVisible ? (
+                        <img
+                            src={upArrow}
+                            alt='Up arrow icon'
+                            onClick={() => toggleDescriptionVisibility()}
+                            style={{ cursor: 'pointer' }}
+                        />
+                    ) : (
+                        <img
+                            src={downArrow}
+                            alt='Down arrow icon'
+                            onClick={() => toggleDescriptionVisibility()}
+                            style={{ cursor: 'pointer' }}
+                        />
+                    )}
+                </div>
+                <div className='roadmap-phase-star-count flex-row'>
+                    <p>{starCount}</p>
+                    {completed ? (
+                        <img src={star} alt='Star icon' />
+                    ) : (
+                        <img src={starEmpty} alt='Star icon' />
+                    )}
+                </div>
+            </div>
+            {isDescriptionVisible && (
+                <div className='activity-description-container'>
+                    <div className='activity-description-content'>
+                        <div className='activity-box flex-row'>
+                            <div className='activity-emoji'>🔑</div>
+                            <div className='flex-col'>
+                                <p>What’s the secret of the perfect cold outreach message? It’s changing the goal to be ‘I want to make this person want to meet me.’ This can be a daunting task, so first we will provide some samples to get your brain rolling! 🧠 </p>
+                            </div>
+                        </div>
+                        <div className='activity-box flex-col'>
+                            <h2>In this activity we will:</h2>
+                            <ul>
+                                <li>Review sample messages in a live form</li>
+                                <li>Rate if you think they would be likely to get a response</li>
+                                <li>Provide thoughts and feedback</li>
+                            </ul>
+                        </div>
+                        <div className='activity-box flex-col'>
+                            <h2>Activity Instructions - Complete Below! 👇🏽</h2>
+                            <TypeformEmbed formId="01JE06YMP90TR719JJF2YKFB44" />
+                        </div>
+                        <div className='activity-box flex-col'>
+                            <h2>Your Turn!</h2>
+                            <p>Your Turn! Write a sample cold call message now. Don’t think about it too much!</p>
+                            <input
+                                type="text"
+                                name="coldMessage"
+                                value={answers.coldMessage}
+                                placeholder='Insert your message here'
+                                onChange={(e) => setAnswers({ ...answers, coldMessage: e.target.value })}
+                            />
+                        </div>
+                        <div className='activity-buttons'>
+                            <div className='activity-button-draft' onClick={() => handleSubmit(false)}>Save as Draft</div>
+                            <div className='activity-button-save' onClick={() => handleSubmit(true)}>Mark as completed!</div>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default RoadmapActivity23;
