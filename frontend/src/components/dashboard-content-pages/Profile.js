a0455245ace9760b378525ccde158e0e0d503ae3
Meet1903 <meet.diwan.md@gmail.com>
2025-01-25 15:17:13 -0500

    Update Profile component to use yellow star icon

--- frontend/src/components/dashboard-content-pages/Profile.js
+++ frontend/src/components/dashboard-content-pages/Profile.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from "react-redux";
-import star from '../../assets/images/star.png';
+import star from '../../assets/images/star-yellow.png';
 
 function Profile({ userId: propUserId }) {
     const dispatch = useDispatch();
