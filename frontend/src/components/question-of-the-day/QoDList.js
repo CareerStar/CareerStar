1c44afb4ed3d486161f400034420d184372e226d
Meet1903 <meet.diwan.md@gmail.com>
2025-01-26 18:40:25 -0500

    Enhance QuestionCard component to include an id prop and improve card interaction with title attribute

--- frontend/src/components/question-of-the-day/QoDList.js
+++ frontend/src/components/question-of-the-day/QoDList.js
@@ -5,11 +5,11 @@ function QoDList({ qod, previousQuestions }) {
     return (
         <div className='qodlist-container'>
             <h2>Today's Question</h2>
-            <QuestionCard question={qod.question} answer={qod.answer} />
+            <QuestionCard question={qod.question} answer={qod.answer} id={1} />
             <h2>Previous Questions</h2>
             <div className='previous-questions'>
                 {previousQuestions.map((q) => (
-                    <QuestionCard key={q.id} question={q.question} answer={q.answer} />
+                    <QuestionCard key={q.id} question={q.question} answer={q.answer} id={2}/>
                 ))}
             </div>
         </div>
