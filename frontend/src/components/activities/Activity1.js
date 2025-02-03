5a82c75e950123ba6e56e3cfa040881a50078651
Meet1903 <meet.diwan.md@gmail.com>
2025-02-02 19:18:43 -0500

    Enhance App.css with responsive design adjustments and add styles for recruiters table

--- frontend/src/components/activities/Activity1.js
+++ frontend/src/components/activities/Activity1.js
@@ -35,8 +35,20 @@ const Activity1 = () => {
     const [starCount, setStarCount] = useState(3);
     const [answers, setAnswers] = useState({
         answer1: '', //Answers can not be a null JSON object, it has to have at least one key-value pair
+        tableData: Array.from({ length: 4 }, () => Array(7).fill("")),
     });
     const [showPopup, setShowPopup] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    const columnHeadings = [
+        "Name",
+        "Company",
+        "Department",
+        "Role",
+        "Time at Company",
+        "LinkedIn",
+        "Role URL",
+    ];
 
     useEffect(() => {
         const fetchUserDetails = async () => {
@@ -44,6 +56,7 @@ const Activity1 = () => {
                 const response = await axios.get(`https://ec2-34-227-29-26.compute-1.amazonaws.com:5000/roadmapactivity/${userId}/${activityId}`);
                 if (response.data) {
                     setAnswers(response.data[0]);
+                    // setTableData(response.data[0].tableData);
                     setCompleted(response.data[1]);
                     setAlreadyCompleted(response.data[1]);
                 }
@@ -57,6 +70,7 @@ const Activity1 = () => {
     }, [userId]);
     const handleSubmit = async (completed) => {
         try {
+            console.log('answers:', answers);
             const payload = {
                 userId: userId,
                 roadmapActivityId: 11,
@@ -83,128 +97,27 @@ const Activity1 = () => {
         }
     };
 
-    const handleInputChange = (name, value) => {
+    const addRow = () => {
         setAnswers((prevAnswers) => ({
             ...prevAnswers,
-            [name]: value,
+            tableData: [...prevAnswers.tableData, Array(7).fill("")],
         }));
     };
 
-    const Step1 = () => (
-        <div className="activity-description">
-            <h2>Step 1 : Understanding Recruiters & Teams</h2>
-            <ol>
-                <li>
-                    <p> Pull up an active job of interest and look for information about the company division,
-                        group or team that you would be working for.</p>
-                    <div className="activity-image">
-                        <img src={step1Image1} alt="Step 1" />
-                    </div>
-                    <div className="activity-hot-tip">
-                        <img src={fireFlameIcon} alt="Fire Flame Icon" />
-                        <p>Hot Tip! Look for things that are in capital letters,
-                            they usually signify a specific department or division within a company!</p>
-                    </div>
-                </li>
-            </ol>
-        </div>
-    );
-
-    const Step2 = () => (
-        <div className="activity-description">
-            <h2>Step 2 : Contact Research & Outreach</h2>
-            <ol>
-                <li><p>Look for information online about this department</p></li>
-                <ul>
-                    <li><p>What other division/department does this roll up to?</p></li>
-                    <li><p>What person is ultimately responsible for this department’s KPIs and Lines of Business? 💯</p></li>
-                    <li><p>What type of division/department would you be working for?</p></li>
-                    <ul>
-                        <li><p>Ex: Engineering, Operations, Customer Applications, Research & Development</p></li>
-                    </ul>
-                </ul>
-                <div className="activity-image">
-                    <img src={step2Image1} alt="Step 1" />
-                </div>
-            </ol>
-        </div>
-    );
-
-    const Step3 = () => (
-        <div className="activity-description">
-            <h2>Step 3 : Tools for networking success</h2>
-            <ol>
-                <li><p>Go to the main company page LinkedIn page. First things first -
-                    follow the company! Now you will see company updates and jobs as they are posted
-                    on your LinkedIn feed!</p>
-                </li>
-                <li><p>You are now going to search people based on parameters from the job posting!
-                    Location - Filter by people that are based in the same city as the job posted
-                    Department Type - See point 2c above! 👔</p>
-                </li>
-                <div className="activity-image">
-                    <img src={step3Image1} alt="Step 1" />
-                </div>
-            </ol>
-        </div>
-    );
-
-    const Step4 = () => {
-        const textareaRef = useRef(null);
-
-        useEffect(() => {
-            textareaRef.current?.focus();
-            const textarea = textareaRef.current;
-            if (textarea) {
-                textarea.focus();
-                textarea.setSelectionRange(textarea.value.length, textarea.value.length);
-            }
-        }, []);
-
-        return (
-            <div className="activity-description">
-                <h2>Step 4 : Your Turn!</h2>
-                <ol>
-                    <li>
-                        <p>Here comes the magic - Now that you have filtered down the results,
-                            look for the team and department that you would be working with among the
-                            people results.
-                        </p>
-                    </li>
-                    <li>
-                        <p>Click on a person and take note of the following:</p>
-                    </li>
-                    <ul>
-                        <li><p>Team Name</p></li>
-                        <li><p>Time at Company</p></li>
-                        <li><p>Have they already posted the role you are looking for or a similar role?</p></li>
-                    </ul>
-                    <textarea
-                        id="notes"
-                        rows="4"
-                        cols="50"
-                        ref={textareaRef}
-                        style={{
-                            width: '100%',
-                            height: '150px',
-                            padding: '10px',
-                            marginBottom: '10px',
-                            border: '1px solid #ccc',
-                            borderRadius: '4px'
-                        }}
-                        placeholder="Write your notes here..."
-                        onChange={(e) => handleInputChange('answer1', e.target.value)}
-                        value={answers.answer1}
-                    />
-                </ol>
-            </div>
-        );
+    const handleTableChange = (rowIndex, colIndex, value) => {
+        setAnswers(prevAnswers => ({
+            ...prevAnswers,
+            tableData: prevAnswers.tableData.map((row, rIdx) =>
+                row.map((cell, cIdx) => (rIdx === rowIndex && cIdx === colIndex ? value : cell))
+            ),
+        }));
     };
+
     const stepsData = [
-        { id: 1, number: "Step 1", title: "Understanding Recruiters and Teams", icon: lawyer, component: <Step1 /> },
-        { id: 2, number: "Step 2", title: "Contact Research & Outreach", icon: caseStudy, component: <Step2 /> },
-        { id: 3, number: "Step 3", title: "Tools for Networking Success", icon: collaboratingInCircle, component: <Step3 /> },
-        { id: 4, number: "Step 4", title: "Your Turn, go shine!", icon: changeUser, component: <Step4 /> },
+        { id: 1, number: "Step 1", title: "Understanding Recruiters and Teams", icon: lawyer },
+        { id: 2, number: "Step 2", title: "Contact Research & Outreach", icon: caseStudy },
+        { id: 3, number: "Step 3", title: "Tools for Networking Success", icon: collaboratingInCircle },
+        { id: 4, number: "Step 4", title: "Your Turn, go shine!", icon: changeUser },
     ];
 
     const scrollToTop = () => {
@@ -247,6 +160,11 @@ const Activity1 = () => {
 
     return (
         <div className="activity-container">
+            {loading && (
+                <div className="spinner-overlay">
+                    <div className="spinner"></div>
+                </div>
+            )}
             {/* Left Section */}
             <div className="activity-container-left-element">
                 <div className="activity-header">
@@ -290,9 +208,111 @@ const Activity1 = () => {
                         <p>It’s important to reach out to more than recruiters and people that have posted the job! Reaching out to the people you could actually be working with is often be much more effective.</p>
 
                     </div>
-                ) : (
-                    <div>{stepsData[currentStep - 1].component}</div>
-                )}
+                ) : currentStep === 1 ? (
+                    <div className="activity-description">
+                        <h2>Step 1 : Understanding Recruiters & Teams</h2>
+                        <ol>
+                            <li>
+                                <p> Pull up an active job of interest and look for information about the company division,
+                                    group or team that you would be working for.</p>
+                                <div className="activity-image">
+                                    <img src={step1Image1} alt="Step 1" />
+                                </div>
+                                <div className="activity-hot-tip">
+                                    <img src={fireFlameIcon} alt="Fire Flame Icon" />
+                                    <p>Hot Tip! Look for things that are in capital letters,
+                                        they usually signify a specific department or division within a company!</p>
+                                </div>
+                            </li>
+                        </ol>
+                    </div>
+                ) : currentStep === 2 ? (
+                    <div className="activity-description">
+                        <h2>Step 2 : Contact Research & Outreach</h2>
+                        <ol>
+                            <li><p>Look for information online about this department</p></li>
+                            <ul>
+                                <li><p>What other division/department does this roll up to?</p></li>
+                                <li><p>What person is ultimately responsible for this department’s KPIs and Lines of Business? 💯</p></li>
+                                <li><p>What type of division/department would you be working for?</p></li>
+                                <ul>
+                                    <li><p>Ex: Engineering, Operations, Customer Applications, Research & Development</p></li>
+                                </ul>
+                            </ul>
+                            <div className="activity-image">
+                                <img src={step2Image1} alt="Step 1" />
+                            </div>
+                        </ol>
+                    </div>
+                ) : currentStep === 3 ? (
+                    <div className="activity-description">
+                        <h2>Step 3 : Tools for networking success</h2>
+                        <ol>
+                            <li><p>Go to the main company page LinkedIn page. First things first -
+                                follow the company! Now you will see company updates and jobs as they are posted
+                                on your LinkedIn feed!</p>
+                            </li>
+                            <li><p>You are now going to search people based on parameters from the job posting!
+                                Location - Filter by people that are based in the same city as the job posted
+                                Department Type - See point 2c above! 👔</p>
+                            </li>
+                            <div className="activity-image">
+                                <img src={step3Image1} alt="Step 1" />
+                            </div>
+                        </ol>
+                    </div>
+                ) : currentStep === 4 ? (
+                    <div className="activity-description">
+                        <h2>Step 4 : Your Turn!</h2>
+                        <ol>
+                            <li>
+                                <p>Here comes the magic - Now that you have filtered down the results,
+                                    look for the team and department that you would be working with among the
+                                    people results.
+                                </p>
+                            </li>
+                            <li>
+                                <p>Click on a person and take note of the following:</p>
+                            </li>
+                            <ul>
+                                <li><p>Team Name</p></li>
+                                <li><p>Time at Company</p></li>
+                                <li><p>Have they already posted the role you are looking for or a similar role?</p></li>
+                            </ul>
+
+                            <div>
+                                <button onClick={addRow}>+</button>
+                                <table className="recruiters-table">
+                                    <thead>
+                                        <tr>
+                                            {columnHeadings.map((heading, colIndex) => (
+                                                <th key={colIndex}>{heading}</th>
+                                            ))}
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {answers.tableData.map((row, rowIndex) => (
+                                            <tr key={rowIndex}>
+                                                {row.map((cell, colIndex) => (
+                                                    <td key={colIndex}>
+                                                        <input
+                                                            type="text"
+                                                            value={cell}
+                                                            onChange={(e) =>
+                                                                handleTableChange(rowIndex, colIndex, e.target.value)
+                                                            }
+                                                        />
+                                                    </td>
+                                                ))}
+                                            </tr>
+                                        ))}
+                                    </tbody>
+
+                                </table>
+                            </div>
+                        </ol>
+                    </div>
+                ) : null}
                 <div className="activity-navigation-buttons">
                     <div className="activity-navigation-back-button" onClick={handleBack} disabled={currentStep === null} style={{ marginRight: "10px" }}>
                         {currentStep === null ? "Back to Activity Page" : "Back to Previous Step"}
