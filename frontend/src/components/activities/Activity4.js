24bb1f0bebc0863b9dc1388a67896e167d75fe5c
Meet1903 <meet.diwan.md@gmail.com>
2025-02-09 17:51:14 -0500

    Add Activity4 component for job listings and user interaction

---
+++ frontend/src/components/activities/Activity4.js
@@ -0,0 +1,262 @@
+import React, { useEffect, useState, useRef } from "react";
+import { useNavigate, useLocation } from 'react-router-dom';
+import lawyer from '../../assets/images/activities/activity1/lawyer.png';
+import backArrow from '../../assets/images/back-arrow.png';
+import activityImage from '../../assets/images/activities/activity4/activity-image.jpg';
+import clock from '../../assets/images/activities/clock.png';
+import star from '../../assets/images/activities/star.png';
+import upArrowScroll from '../../assets/images/up-arrow-scroll.png';
+import likeIcon from '../../assets/images/like-icon.png';
+import dislikeIcon from '../../assets/images/dislike-icon.png';
+import JobListing from "./activities-support/JobListing";
+
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
+
+
+const Activity4 = () => {
+    const navigate = useNavigate();
+    const location = useLocation();
+    const prevPage = location.state?.prevPage;
+    const [currentStep, setCurrentStep] = useState(null); // null shows activity description by default
+
+    const dispatch = useDispatch();
+    const userId = localStorage.getItem('userId');
+    // const history = useHistory();
+    const activityId = 3;
+    const [completed, setCompleted] = useState(false);
+    const [alreadyCompleted, setAlreadyCompleted] = useState(false);
+    const [starCount, setStarCount] = useState(3);
+    const [answers, setAnswers] = useState({
+        answer1: '',
+    });
+    const [showHotTipPopup, setShowHotTipPopup] = useState(false);
+    const [showLikeDislikePopup, setShowLikeDislikePopup] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    const jobs = [
+        { title: "Full-Stack AI Developer", company: "Ringy", url: "https://www.linkedin.com/jobs/view/4139911683/" },
+        { title: "Software Development in Test Intern", company: "Zoox", url: "https://www.linkedin.com/jobs/view/4126800148/" },
+        { title: "Full Stack Engineer", company: "Sensor Tower", url: "https://www.linkedin.com/jobs/view/4137264359/" },
+        { title: "UI Developer Intern", company: "Copart", url: "https://www.linkedin.com/jobs/view/4127710370/" },
+        { title: "Software Engineer II", company: "Cadence", url: "https://www.linkedin.com/jobs/view/4081360031/" },
+        { title: "Data Scientist Intern", company: "Granite Telecommunications", url: "https://www.linkedin.com/jobs/view/4105424381/" },
+        { title: "Airline Data Analyst - 2025 Summer Intern", company: "Sabre Corporation", url: "https://www.linkedin.com/jobs/view/4117473265/" },
+        { title: "Production Engineer (Early Career)", company: "Ingevity", url: "https://www.linkedin.com/jobs/view/4117456634/" },
+    ];
+
+    const popupRef = useRef(null);
+
+    const closePopUp = () => {
+        setShowHotTipPopup(false);
+    }
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (popupRef.current && !popupRef.current.contains(event.target)) {
+                closePopUp();
+            }
+        };
+
+        document.addEventListener('mousedown', handleClickOutside);
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [popupRef]);
+
+    useEffect(() => {
+        const fetchUserDetails = async () => {
+            try {
+                const response = await axios.get(`https://api.careerstar.co/roadmapactivity/${userId}/${activityId}`);
+                if (response.data) {
+                    setAnswers(response.data[0]);
+                    setCompleted(response.data[1]);
+                    setAlreadyCompleted(response.data[1]);
+                }
+            } catch (error) {
+                console.error('Activity not completed', error);
+            }
+        };
+        if (userId) {
+            fetchUserDetails();
+        }
+    }, [userId]);
+
+    const handleSubmit = async (completed) => {
+        try {
+            const payload = {
+                userId: userId,
+                completed: completed,
+                answers: answers,
+                stars: starCount,
+            };
+            const response = await axios.post(`https://api.careerstar.co/roadmapactivity/${userId}/${activityId}`, payload);
+            if (response.status === 200) {
+                if (completed) {
+                    setCompleted(true);
+                }
+                if (completed && !alreadyCompleted) {
+                    dispatch({ type: "INCREMENT_STAR", payload: starCount });
+                }
+            }
+            if (prevPage === 'roadmap') {
+                navigate('/dashboard/roadmap');
+            } else {
+                navigate('/dashboard/home');
+            }
+        } catch (error) {
+            console.error('Error saving answers:', error);
+        }
+    };
+
+    const [votes, setVotes] = useState({});
+
+    const handleVote = (id, isGood) => {
+        setVotes(prevVotes => ({ ...prevVotes, [id]: isGood }));
+    };
+
+    const stepsData = [
+        { id: 1, number: "Step 1", title: "Hot Jobs", icon: lawyer },
+    ];
+
+    const scrollToTop = () => {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    };
+
+    const handleStepChange = (stepId) => {
+        scrollToTop();
+        setCurrentStep(stepId);
+    };
+
+    const handleNext = () => {
+        scrollToTop();
+        if (currentStep === null) {
+            setCurrentStep(1);
+        } else if (currentStep === stepsData.length) {
+            setShowLikeDislikePopup(true);
+        } else if (currentStep < stepsData.length) {
+            setCurrentStep(currentStep + 1);
+        }
+    };
+
+    const handleBack = () => {
+        scrollToTop();
+        if (currentStep === 1) {
+            setCurrentStep(null); // Go back to activity description
+        } else if (currentStep > 1) {
+            setCurrentStep(currentStep - 1);
+        } else if (currentStep === null) {
+            if (prevPage === 'roadmap') {
+                navigate('/dashboard/roadmap');
+            } else {
+                navigate('/dashboard/home');
+            }
+        }
+    };
+
+    const handleBackNavigation = () => {
+        if (prevPage === 'roadmap') {
+            navigate('/dashboard/roadmap');
+        } else {
+            navigate('/dashboard/home');
+        }
+    }
+
+    return (
+        <div className="activity-container">
+            {loading && (
+                <div className="spinner-overlay">
+                    <div className="spinner"></div>
+                </div>
+            )}
+            {/* Left Section */}
+            <div className="activity-container-left-element">
+                <div className="activity-header">
+                    <img src={backArrow} alt="Back" onClick={handleBackNavigation} />
+                    <h3>Activity Steps :</h3>
+                </div>
+                <div className="activity-step-container">
+                    {stepsData.map((step) => (
+                        <div className="activity-step">
+                            <div className="activity-step-left-element">
+                                <img src={step.icon} alt="Case Study" />
+                            </div>
+                            <div className="activity-step-right-element">
+                                <div className="activity-step-number">{step.number}</div>
+                                <div className="activity-step-title">{step.title}</div>
+                                <div className={`activity-step-button ${currentStep === step.id ? "selected" : ""}`} onClick={() => handleStepChange(step.id)}>Dive In</div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+            {/* Right Section */}
+            <div className="activity-container-right-element">
+                <img src={upArrowScroll} alt="Scroll to Top" className="scroll-top" onClick={scrollToTop} />
+                {currentStep === null ? (
+                    <div className="activity-description">
+                        <div className="activity-main-image">
+                            <img src={activityImage} alt="Activity" />
+                        </div>
+                        <h2>Hot Jobs of the Week!</h2>
+                        <div className="inline-container">
+                            <div className="inline-container">
+                                <img src={clock} alt="Clock" />
+                                <p>10 minutes</p>
+                            </div>
+                            <div className="inline-container">
+                                <img src={star} alt="Star" />
+                                <p>{starCount} stars</p>
+                            </div>
+                        </div>
+                        <p>Check out these hot jobs of the week!</p>
+
+                    </div>
+                ) : currentStep === 1 ? (
+                    <div className="activity-description">
+                        <h2>Step 1 : Hot Jobs</h2>
+                        {jobs.map((job, index) => (
+                            <JobListing key={index} {...job} />
+                        ))}
+                    </div>
+                ) : null}
+                <div className="activity-navigation-buttons">
+                    <div className="activity-navigation-back-button" onClick={handleBack} disabled={currentStep === null} style={{ marginRight: "10px" }}>
+                        {currentStep === null ? "Back to Activity Page" : "Back to Previous Step"}
+                    </div>
+                    <div className="activity-navigation-next-button" onClick={handleNext} disabled={currentStep === stepsData.length}>
+                        {currentStep === stepsData.length ? "Mark as completed" : "Go to Next Step"}
+                    </div>
+                </div>
+            </div>
+
+            {showLikeDislikePopup && (
+                <div className='activity-like-dislike-popup'>
+                    <div className='activity-like-dislike-popup-content'>
+                        <p>How did you find this activity?</p>
+                        <div className='activity-like-dislike-buttons'>
+                            <div className='activity-like-button' onClick={() => handleSubmit(true)}>
+                                <img src={likeIcon} alt="Like" />
+                            </div>
+                            <div className='activity-dislike-button' onClick={() => handleSubmit(true)}>
+                                <img src={dislikeIcon} alt="Dislike" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+
+            {showHotTipPopup && (
+                <div className='hot-tip-popup'>
+                    <div className='hot-tip-popup-content' ref={popupRef}>
+                        {/* <img src={step1Image2} alt='3 stars' /> */}
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default Activity4;
