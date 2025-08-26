e483aa93c10dc81979316c37fcb198672a8f552d
mdhnguyen8 <mdhnguyen8@gmail.com>
2025-08-26 13:14:32 -0400

    Revert "Branch navarro"

--- frontend/src/components/activities/activities-support/FlippableCard.js
+++ frontend/src/components/activities/activities-support/FlippableCard.js
@@ -1,26 +1,20 @@
 import { useState } from "react";
-import flippableCardFront from "../../../assets/images/activities/front.png";
-import flippableCardBack from "../../../assets/images/activities/back.png";
+import flippableCardFront from "../../../assets/images/activities/flippable-card-front.png";
+import flippableCardBack from "../../../assets/images/activities/flippable-card-back.png";
 
 export default function FlippableCard({ abbreviation, fullForm, duty }) {
   const [isFlipped, setIsFlipped] = useState(false);
 
   return (
     <div className="card-container" onClick={() => setIsFlipped(!isFlipped)}>
-      <div className={`card ${isFlipped ? "flipped" : ""}`}>
+      <div className={`card ${isFlipped ? 'flipped' : ''}`}>
         {/* Front Side */}
-        <div
-          className="card-front"
-          style={{ backgroundImage: `url(${flippableCardFront})` }}
-        >
+        <div className="card-front" style={{ backgroundImage: `url(${flippableCardFront})` }}>
           <p className="card-text">{abbreviation}</p>
         </div>
 
         {/* Back Side */}
-        <div
-          className="card-back"
-          style={{ backgroundImage: `url(${flippableCardBack})` }}
-        >
+        <div className="card-back" style={{ backgroundImage: `url(${flippableCardBack})` }}>
           <p className="card-title">{fullForm}</p>
           <p className="card-duty">{duty}</p>
         </div>
